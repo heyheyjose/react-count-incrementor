79fadfbb08797ec4db74f522651deb8ec0762831
heyheyjose <heyheyjose@users.noreply.github.com>
2015-07-18 18:35:55 -0400

    finished up incrementing functionality and styling

--- src/app.jsx
+++ src/app.jsx
@@ -2,9 +2,24 @@ var React = require('react');
 
 var Counter = React.createClass({
 
+	getInitialState: function () {
+		return {
+			count: 0
+		}
+	},
+
+	incrementCount: function () {
+		this.setState({
+			count: this.state.count + 1
+		});
+	},
+
 	render: function () {
 		return (
-			<div>this will be the incrementor</div>
+			<div className="counter-component">
+				<h1>Count: {this.state.count}</h1>
+				<button type="button" onClick={this.incrementCount}>Increment</button>
+			</div>
 		);
 	}
 
